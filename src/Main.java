448a0f842d07b6ba9ff905af323a3e234fc8ed82
Sergey Starodubcev <starodubcev_2018@list.ru>
2024-09-17 23:51:04 +0300

    Third version game "Gallows"

--- src/Main.java
+++ src/Main.java
@@ -22,14 +22,47 @@ public class Main {
     };
 
     public static void main(String[] args) {
-        wordSecret = "Слово";
-        wordCloneForLetter = wordSecret;
-        startedGame();
+        startGame();
     }
 
-    public static void startedGame() {
+    public static void startGame(){
+        wordSecret = inputSecretWordFirstPlayer();
+        wordCloneForLetter = wordSecret;
+        CheckingTheLetter();
+    }
 
-        String letter = CheckongTheLetter();
+    public static String CheckingTheLetter() {
+        int i = 0;
+        StringBuilder stringBuilder = new StringBuilder(isGetCloneWord());
+        displayGallow();
+        System.out.println("\nСлово:" + isGetCloneWord());
+        while (i <= MAXTRY) {
+            boolean foundLetter = false;
+            inputLetterPlayer = inputPlayerTheLetter();
+            for (int j = 0; j < wordSecret.length(); j++) {
+                if (wordSecret.charAt(j) == inputLetterPlayer.charAt(0)) {
+                    foundLetter = true;
+                    System.out.println("Вы угадали букву!");
+                    stringBuilder.setCharAt(j, inputLetterPlayer.charAt(0));
+                    recordWord = stringBuilder.toString();
+                    System.out.println("Слово:" + recordWord);
+                    System.out.println("Ошибки:" + lettersError.toString());
+                }
+            }
+            if (!foundLetter) {
+                System.out.println("Вы не угадали букву!");
+                System.out.println("Слово:" + isGetCloneWord());
+                ++i;
+                lettersError.add(inputLetterPlayer);
+                System.out.println("Ошибки:" + lettersError.toString());
+                outputGallow(i);
+            }
+
+            checkingWords(recordWord);
+            checkingTheAttempt(i);
+
+        }
+        return recordWord;
     }
 
     public static void displayGallow() {
@@ -79,49 +112,11 @@ public class Main {
         return inputLetterPlayer;
     }
 
-    public static String CheckongTheLetter() {
-        int i = 0;
-        StringBuilder stringBuilder = new StringBuilder(isGetCloneWord());
-        displayGallow();
-        System.out.println("\nСлово:"+isGetCloneWord());
-        while (i <= MAXTRY) {
-            boolean foundLetter = false;
-            inputLetterPlayer = inputPlayerTheLetter();
-            for (int j = 0; j < wordSecret.length(); j++) {
-                if (wordSecret.charAt(j) == inputLetterPlayer.charAt(0)) {
-                    foundLetter = true;
-                    System.out.println("Вы угадали букву!");
-                    stringBuilder.setCharAt(j, inputLetterPlayer.charAt(0));
-                    recordWord = stringBuilder.toString();
-                    System.out.println("Слово:"+recordWord);
-                    System.out.println("Ошибки:"+ lettersError.toString());
-                }
-            }
-            if (!foundLetter) {
-                System.out.println("Вы не угадали букву!");
-                System.out.println("Слово:"+recordWord);
-                ++i;
-                lettersError.add(inputLetterPlayer);
-                System.out.println("Ошибки:"+ lettersError.toString());
-                outputGallow(i);
-            }
-
-
-            if (i == MAXTRY) {
-                System.out.println("\nОтгадывающий проиграл, загадонное слово" + "-" + wordSecret);
-                break;
-            }
-        }
-        return recordWord;
-        //Проверяется условие на совпадение буквы в слове
-        //цикл while с условием когда ошибок не должно быть больше 6
+    public static String isGetCloneWord() {
+        return wordCloneForLetter.replaceAll("[а-яa-zA-ZА-Я]", "*");
     }
 
-    public static String isGetCloneWord(){
-        return  wordCloneForLetter.replaceAll("[а-яa-zA-ZА-Я]", "*");
-    }
-
-    public static boolean сheckingWords(){
+    public static boolean сheckingWords() {
         if (recordWord.equals(wordSecret)) {
             System.out.println("\nВы угадали слово, поздравляем!!!");
             return true;
@@ -129,11 +124,70 @@ public class Main {
         return false;
     }
 
-//    public static String inputPlayerTheNumber() {
-//        //Игрок 2 вводит букву через scanner
-//
-//        int i = scanner.next();
-//        return i;
-//    }
+    public static int inputPlayerTheNumber() {
+        //Игрок 2 вводит букву через scanner
+        System.out.println("Введите цифру:");
+        int i = Integer.parseInt(scanner.next());
+        return i;
+    }
 
+    public static boolean checkingWords(String recordWord) {
+        if (wordSecret.equals(recordWord)) {
+            System.out.println("\nВы угадали слово, поздравляем!!!");
+            restartExitGame();
+            return true;
+
+        }
+        return false;
+    }
+
+    public static boolean checkingTheAttempt(int attempt) {
+        if (attempt == MAXTRY) {
+            System.out.println("\nОтгадывающий проиграл, загадонное слово" + "-" + wordSecret);
+            restartExitGame();
+            return true;
+
+        }
+        return false;
+    }
+
+    public static ArrayList<String> zeroingTheArray() {
+        for (int i = lettersError.size() - 1; i >= 0; i--) {
+            lettersError.remove(i);
+        }
+        return lettersError;
+    }
+
+    public static String[] zeroingTheGallows() {
+        glawos = new String[]{
+                " _____\n",
+                " |   |\n",
+                " |   \n",
+                " |   \n",
+                " |   \n",
+                "-----------"
+        };
+        return glawos;
+    }
+
+    public static void restartExitGame(){
+        System.out.println("Вы хотите повторить игру?");
+        System.out.println("Выйти из игры-нажмите на число 1");
+        System.out.println("Повторить игру-нажмите на число 2");
+        int i=inputPlayerTheNumber();
+        if (i==1){
+            System.exit(0);
+        }
+        if (i==2){
+            zeroingTheArray();
+            zeroingTheGallows();
+            startGame();
+        }
+    }
+
+    public static String inputSecretWordFirstPlayer() {
+        System.out.println("Введи секретное слово:");
+        String secretWord = scanner.next();
+        return secretWord;
+    }
 }
